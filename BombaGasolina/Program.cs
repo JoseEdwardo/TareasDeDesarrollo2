2e28c7977ea99d0585e35c44623b2e0591119467
hilary231 <1125701@est.intec.edu.do>
2025-04-25 10:09:35 -0400

    Tercera Modificacion

---
+++ BombaGasolina/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace BombaGasolina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables para almacenar el precio del galon de combustible
+            //y la cantidad de gasolina en el tanque
+
+            double precio = 2.00;
+            double cantEnELTanque = 300.00;
+            
+            //imprimir el mensaje inicial
+            Console.WriteLine($"El Tanque de Gasolina tiene {cantEnELTanque} galones");
+            Console.WriteLine($"El Precio por galón de gasolina es de {precio:C}");
+            Console.WriteLine();
+
+            double solicitud;
+            double cantDespachada;
+            double valorTransaccion;
+            
+            while(true)
+            {
+                Console.Write("Introduzca la cantidad de galones que desea solicitar: ");
+                solicitud = Convert.ToDouble(Console.ReadLine());
+
+                //determinar si hay suficiente combustible
+                if (cantEnELTanque >= solicitud)
+                {
+                    cantDespachada = solicitud;
+                    cantEnELTanque = cantEnELTanque - cantDespachada;
+                    valorTransaccion = precio * cantDespachada;
+                }
+                else
+                {
+                    cantDespachada = cantEnELTanque;
+                    cantEnELTanque = cantEnELTanque - cantDespachada;
+                    valorTransaccion = precio * cantDespachada;
+                }
+
+                Console.WriteLine($"{solicitud:F2} galones fueron solicitados");
+                Console.WriteLine($"{cantDespachada:F2} galones fueron surtidos");
+                Console.WriteLine($"{cantEnELTanque:F2} quedan en el tanque");
+                Console.WriteLine($"El precio total es {valorTransaccion:C}");
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ EjercicioDeElevador/Program.cs
@@ -0,0 +1,46 @@
+﻿int pisoActual = 1; //Aqui se declaran las variables...
+int pisoDestino;
+
+while (true)
+{
+    Console.WriteLine($"Se encuentra en el piso: {pisoActual}");
+    Console.WriteLine("A que piso desea moverse?: ");
+
+    int.TryParse(Console.ReadLine(), out pisoDestino);
+
+    //Validaciones
+    if (pisoDestino > 15 || pisoDestino < 1)
+    {
+        Console.WriteLine("Piso invalido, intente de nuevo");
+        continue;
+    }
+
+    if (pisoDestino == pisoActual)
+    {
+        Console.WriteLine("Ya estas en este piso");
+        continue;
+    }
+
+    // Logica del ascensor
+    //Subida
+    if (pisoDestino > pisoActual)
+    {
+        while (pisoActual != pisoDestino)
+        {
+            pisoActual += 1;
+            Console.WriteLine($"subiendo, ahora esta en el piso: {pisoActual}");
+        }
+    }
+    //Bajada
+    else
+    {
+        while (pisoActual != pisoDestino)
+        {
+            pisoActual -= 1;
+            Console.WriteLine($"bajando, ahora esta en el piso: {pisoActual}");
+        }
+    }
+    Console.ReadKey();
+    Console.Clear();
+}
+
